71c7e496c2d52e0d8874899ae9c39e1e0b5b8e65
Fabian Hohmuth <fafa3711@users.noreply.github.com>
2024-01-23 16:42:44 +0100

    default 3d point size

--- F3H.ProfileShark/RawBoard3D/I3DDisplaySettings.cs
+++ F3H.ProfileShark/RawBoard3D/I3DDisplaySettings.cs
@@ -9,7 +9,7 @@ public interface I3DDisplaySettings : INotifyPropertyChanged
     [Option(DefaultValue = true)] bool ShowRawPoints { get; set; }
     [Option(DefaultValue = true)] bool ShowDebugStuff { get; set; }
     [Option(DefaultValue = "ByIntensity")] DisplayMode DisplayMode { get; set; }
-    [Option(DefaultValue = "1.0")] double RawPointSize { get; set; }
+    [Option(DefaultValue = "2.0")] double RawPointSize { get; set; }
     [Option(DefaultValue = "#ffda00")] Color CameraAColor { get; set; }
     [Option(DefaultValue = "Purple")] Color CameraBColor { get; set; }
     [Option(DefaultValue = false)] bool OrthographicCamera { get; set; }
